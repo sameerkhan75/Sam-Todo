283d97bb70f8e53b6124f0746a25b24187006c43
samee <sameer754811@gmail.com>
2025-06-19 16:24:18 +0530

    fix

--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AddTodo = () => {
+const AddTodo = (props) => {
+    const [title,setTitle]= useState("");
+    const [desc,setDesc]= useState("");
+    const submit=()=>{
+
+    }
   return (
     <div className="container my-3">
         <h3>Add a Todo</h3>
-      <form>
+      <form onSubmit={submit}>
   <div class="mb-3">
     <label for="title" class="form-label">Todo Title</label>
-    <input type="text" class="form-control" id="title" aria-describedby="add your todo plz"/>
+    <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} class="form-control" id="title" aria-describedby="add your todo plz"/>
   </div>
   <div class="mb-3">
     <label for="desc" class="form-label">Todo Description</label>
-    <input type="text" class="form-control" id="desc"/>
+    <input type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} class="form-control" id="desc"/>
   </div>
 
   <button type="submit" class="btn btn-sm btn-success">AddTodo</button>
