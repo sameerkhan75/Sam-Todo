f6384cf49832e231dc76866174982c19f24c3559
samee <sameer754811@gmail.com>
2025-06-23 14:27:20 +0530

    update

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -12,7 +12,7 @@ const About = () => {
         
       </p>
       <p>
-        It has clean interface so that you won't have to worry about unneccessary features
+        It has clean interface so that you won't have to worry about unneccessary features. you can add images with the items
       </p>
     </div>
   );
